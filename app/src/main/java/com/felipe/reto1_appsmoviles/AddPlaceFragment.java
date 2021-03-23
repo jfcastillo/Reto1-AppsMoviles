0c477f06ff51ded6efa4c58a9ad699fb8f5ae9db
Felipe Castillo <jfcastillo1204@gmail.com>
2021-03-22 21:32:22 -0500

    navigator and new place fragment working

---
+++ app/src/main/java/com/felipe/reto1_appsmoviles/AddPlaceFragment.java
@@ -0,0 +1,35 @@
+package com.felipe.reto1_appsmoviles;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AddPlaceFragment extends Fragment {
+
+
+    public AddPlaceFragment() {
+        // Required empty public constructor
+    }
+
+
+    // TODO: Rename and change types and number of parameters
+    public static AddPlaceFragment newInstance() {
+        AddPlaceFragment fragment = new AddPlaceFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_place, container, false);
+    }
+}
--- app/src/main/java/com/felipe/reto1_appsmoviles/MainActivity.java
+++ app/src/main/java/com/felipe/reto1_appsmoviles/MainActivity.java
@@ -1,14 +1,57 @@
 package com.felipe.reto1_appsmoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AddPlaceFragment addplace;
+    private MapFragment mapFragment;
+    private SearchPlacesFragment searchPlacesFragment;
+    private BottomNavigationView navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addplace = AddPlaceFragment.newInstance();
+        mapFragment = MapFragment.newInstance();
+        searchPlacesFragment = SearchPlacesFragment.newInstance();
+
+        navigator = findViewById(R.id.navigator);
+        navigator.setOnNavigationItemSelectedListener(
+                (menuItem ) ->{
+                    switch (menuItem.getItemId()){
+                        case R.id.newPlace:
+                            showFragment(addplace);
+                            break;
+                        case R.id.map:
+                            showFragment(mapFragment);
+                            break;
+                        case R.id.search:
+                            showFragment(searchPlacesFragment);
+                            break;
+                    }
+
+                    return true;
+
+                }
+        );
+
+        showFragment(addplace);
+    }
+
+    public void showFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.commit();
     }
 }
