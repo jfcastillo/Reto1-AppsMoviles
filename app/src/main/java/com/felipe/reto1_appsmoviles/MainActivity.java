a7412d01cef8ef570f6f309ca1882fe1af2666cc
Felipe Castillo <jfcastillo1204@gmail.com>
2021-03-22 19:42:31 -0500

    Project created

---
+++ app/src/main/java/com/felipe/reto1_appsmoviles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.felipe.reto1_appsmoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
